22f25f3b999bed1112dd61157ee158af2f13148e
unknown <angelesianlloyd_bist@gmail.com>
2025-02-19 15:38:13 +0800

    Submitting Python Week 04 - Laboratory # 13

---
+++ for_loop_break_continue.py
@@ -0,0 +1,17 @@
+# Ian Lloyd Santiago Angeles
+# ITELEC2
+# Laboratory #13 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 11]
+    for num in numbers:
+        if num == 3:
+            continue  # Skip the rest of this iteration
+        if num == 7:
+            break  # Exit the loop completely
+        print(num)
+
+if __name__ == "__main__":
+    main()
